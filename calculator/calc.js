023133050ed70c8ccbd7dc7dd48904e637a3335d
Mike Chalmers <mikechalmers@gmail.com>
2020-09-09 14:58:46 +0100

    inital calculator coding

---
+++ calculator/calc.js
@@ -0,0 +1,30 @@
+var display = document.querySelector("#display");
+
+var c = document.querySelector("#C");
+var ce = document.querySelector("#CE");
+
+var percent = document.querySelector("#percent");
+var divide = document.querySelector("#divide");
+var times = document.querySelector("#times");
+var minus = document.querySelector("#minus");
+var plus = document.querySelector("#plus");
+var equals = document.querySelector("#equals");
+
+var one = document.querySelector("#one");
+var two = document.querySelector("#two");
+var three = document.querySelector("#three");
+var four = document.querySelector("#four");
+var five = document.querySelector("#five");
+var six = document.querySelector("#six");
+var seven = document.querySelector("#seven");
+var eight = document.querySelector("#eight");
+var nine = document.querySelector("#nine");
+var zero = document.querySelector("#zero");
+
+c.addEventListener("click", function(e){
+  display.value = "hello";
+});
+
+// if button is pressed
+// take its value
+// do something with it
