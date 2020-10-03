5e10e88a24ea1a4e814a19be9a27dc92b43375d5
Mike Chalmers <mikechalmers@gmail.com>
2020-10-03 12:33:54 +0100

    Add strobing to Christmas Lights

--- christmas-lights/xmas.js
+++ christmas-lights/xmas.js
@@ -2,6 +2,8 @@ var bulbs = document.querySelectorAll('.bulb');
 // var swtch = document.querySelector('#power');
 var radios = document.querySelectorAll('input[name="intensity"]');
 
+var blinkers = document.querySelectorAll('input[name="freq"]');
+
 // swtch.addEventListener('click', (event) => {
 //   bulbs.forEach(function(bulb){
 //
@@ -39,6 +41,42 @@ if(radios) {
       } else if (item === "off") {
         bulbs.forEach(function(bulb){
           settingOff(bulb);
+        })
+      }
+
+    })
+  });
+}
+
+if(blinkers) {
+  blinkers.forEach((radio) => {
+    radio.addEventListener("change", function(event){
+      var item = event.target.value;
+
+      if(item === "1") {
+        alert("works");
+        bulbs.forEach(function(bulb){
+          settingFreqOne(bulb);
+        })
+      } else if (item === "2") {
+        bulbs.forEach(function(bulb){
+          settingFreqTwo(bulb);
+        })
+      } else if (item === "3") {
+        bulbs.forEach(function(bulb){
+          settingFreqThree(bulb);
+        })
+      } else if (item === "4") {
+        bulbs.forEach(function(bulb){
+          settingFreqFour(bulb);
+        })
+      } else if (item === "5") {
+        bulbs.forEach(function(bulb){
+          settingFreqFive(bulb);
+        })
+      } else if (item === "off") {
+        bulbs.forEach(function(bulb){
+          settingFreqOff(bulb);
         })
       }
 
@@ -102,6 +140,56 @@ function settingOff(bulb) {
     bulb.setAttribute('style', 'box-shadow: inset 0 0 15px rgba(255, 255, 255, 0), inset 20px 0 20px rgba(255, 0, 0, 0), inset -20px 0 20px rgba(255, 153, 153, 0), inset 20px 0 40px rgba(255, 0, 0, 0), inset -20px 0 40px rgba(255, 153, 153, 0), 0 0 10px rgba(255, 255, 255, 0), -10px 0 30px rgba(255, 0, 0, 0), 0 0 50px 20px rgba(255, 0, 0, 0), 10px 0 30px 10px rgba(255, 153, 153, 0)');
   } else if(bulb.classList.contains('blue')) {
     bulb.setAttribute('style', 'box-shadow: inset 0 0 15px rgba(255, 255, 255, 0), inset 20px 0 20px rgba(0, 0, 255, 0), inset -20px 0 20px rgba(153, 153, 255, 0), inset 20px 0 40px rgba(0, 0, 255, 0), inset -20px 0 40px rgba(153, 153, 255, 0), 0 0 10px rgba(255, 255, 255, 0), -10px 0 30px rgba(0, 0, 255, 0), 0 0 50px 20px rgba(0, 0, 255, 0), 10px 0 30px 10px rgba(153, 153, 255, 0)');
+  }
+
+}
+
+// FREQUENCY
+
+function settingFreqOne(bulb) {
+
+
+
+}
+
+function settingFreqTwo(bulb) {
+
+
+
+}
+
+function settingFreqThree(bulb) {
+
+
+
+}
+
+function settingFreqFour(bulb) {
+
+
+
+}
+
+function settingFreqFive(bulb) {
+
+  if(bulb.classList.contains('red')) {
+    bulb.classList.add('blinkRed');
+  } else if(bulb.classList.contains('blue')) {
+    bulb.classList.add('blinkBlue');
+  }
+
+}
+
+function settingFreqOff(bulb) {
+
+  if(bulb.classList.contains('red')) {
+    if(bulb.classList.contains('blinkRed')) {
+      bulb.classList.remove('blinkRed');
+    }
+  } else if(bulb.classList.contains('blue')) {
+    if(bulb.classList.contains('blinkBlue')) {
+      bulb.classList.remove('blinkBlue');
+    }
   }
 
 }
